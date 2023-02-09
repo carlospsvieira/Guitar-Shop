cd2e45b31b276fadc5b61a2229585eb258d08bf4
Carlos Vieira <carlepsvieira@gmail.com>
2023-02-09 10:51:11 -0300

    adds ids to guitars data

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,6 @@
 export default guitars = [
   {
+    id: "0312",
     name: "Martin D-28",
     brand: "Martin",
     price: 3999.99,
@@ -10,36 +11,43 @@ export default guitars = [
       "The Martin D-28 is a classic acoustic guitar with a rich sound and warm tone. Ideal for musicians of all styles and genres.",
   },
   {
+    id: "0313",
     name: "Taylor 314ce",
     brand: "Taylor",
     price: 1999.99,
     type: "acoustic",
-    image: "https://1.bp.blogspot.com/-V8g9kkpdZNg/XXU4qk3o4DI/AAAAAAABFLo/xJ5EFgSp_407seNRyi5wr7uHqo6nbQdVACLcBGAs/s1600/314ce.jpg",
+    image:
+      "https://1.bp.blogspot.com/-V8g9kkpdZNg/XXU4qk3o4DI/AAAAAAABFLo/xJ5EFgSp_407seNRyi5wr7uHqo6nbQdVACLcBGAs/s1600/314ce.jpg",
     shortDescription: "High-quality acoustic guitar.",
     longDescription:
       "The Taylor 314ce is a versatile acoustic guitar with a bright, balanced sound and comfortable playability. Perfect for songwriting and performing.",
   },
   {
+    id: "0314",
     name: "Gibson J-45",
     brand: "Gibson",
     price: 2499.99,
     type: "acoustic",
-    image: "https://www.royalmusic.com.br/wp-content/uploads/2018/02/Gibson-J-45-Standard-2018-Vintage-Sunburst-01-1000x564.jpg",
+    image:
+      "https://www.royalmusic.com.br/wp-content/uploads/2018/02/Gibson-J-45-Standard-2018-Vintage-Sunburst-01-1000x564.jpg",
     shortDescription: "Iconic acoustic guitar.",
     longDescription:
       "The Gibson J-45 is a classic acoustic guitar known for its warm, rich sound and timeless design. Ideal for folk and blues musicians.",
   },
   {
+    id: "0315",
     name: "Yamaha FG830",
     brand: "Yamaha",
     price: 599.99,
     type: "acoustic",
-    image: "https://d1aeri3ty3izns.cloudfront.net/media/59/599948/1200/preview.jpg",
+    image:
+      "https://d1aeri3ty3izns.cloudfront.net/media/59/599948/1200/preview.jpg",
     shortDescription: "Affordable, quality acoustic guitar.",
     longDescription:
       "The Yamaha FG830 is an affordable acoustic guitar with a balanced, rich sound and excellent playability. Ideal for beginners and experienced players alike.",
   },
   {
+    id: "0316",
     name: "Seagull S6 Original",
     brand: "Seagull",
     price: 449.99,
@@ -50,76 +58,91 @@ export default guitars = [
       "The Seagull S6 Original is a well-crafted acoustic guitar with a rich, balanced sound and comfortable playability. Perfect for a range of styles and genres.",
   },
   {
+    id: "0317",
     name: "Epiphone EJ-200CE",
     brand: "Epiphone",
     price: 499.99,
     type: "acoustic",
-    image: "https://imageswscdn.wslojas.com.br/files/7106/violao-epiphone-ej-200-sce-vn-eletrico-tampo-macico4.jpg",
+    image:
+      "https://imageswscdn.wslojas.com.br/files/7106/violao-epiphone-ej-200-sce-vn-eletrico-tampo-macico4.jpg",
     shortDescription: "Affordable jumbo acoustic guitar.",
     longDescription:
       "The Epiphone EJ-200CE is a budget-friendly jumbo acoustic guitar with a warm, rich sound and easy playability. Ideal for musicians of all levels.",
   },
   {
+    id: "0320",
     name: "Takamine GD71CE",
     brand: "Takamine",
     price: 799.99,
     type: "acoustic",
-    image: "https://images.tcdn.com.br/img/img_prod/682336/violao_takamine_gd71ce_e_tk_40_natural_eletrico_aco_341_2_20190515162142.jpg",
+    image:
+      "https://images.tcdn.com.br/img/img_prod/682336/violao_takamine_gd71ce_e_tk_40_natural_eletrico_aco_341_2_20190515162142.jpg",
     shortDescription: "High-quality dreadnought acoustic guitar.",
     longDescription:
       "The Takamine GD71CE is a well-made dreadnought acoustic guitar with a balanced, powerful sound and excellent playability. Perfect for rock, country, and blues musicians.",
   },
   {
+    id: "0321",
     name: "Fender Stratocaster",
     brand: "Fender",
     price: 999.99,
     type: "electric",
-    image: "https://www.lojaopenstage.com.br/upload/product/d4riMsuuP2HYRPMFmKCPFFlr29m8EZwitdzEwVUj.jpg",
+    image:
+      "https://www.lojaopenstage.com.br/upload/product/d4riMsuuP2HYRPMFmKCPFFlr29m8EZwitdzEwVUj.jpg",
     shortDescription: "Classic electric guitar.",
     longDescription:
       "The Fender Stratocaster is a classic electric guitar with a fast, comfortable neck and versatile sound. Ideal for a range of musical styles.",
   },
   {
+    id: "0322",
     name: "Gibson SG Standard",
     brand: "Gibson",
     price: 1499.99,
     type: "electric",
-    image: "https://www.kennysmusic.co.uk/images/gibson-sg-standard-61-vintage-cherry-p2293-5033_image.jpg",
+    image:
+      "https://www.kennysmusic.co.uk/images/gibson-sg-standard-61-vintage-cherry-p2293-5033_image.jpg",
     shortDescription: "Legendary electric guitar.",
     longDescription:
       "The Gibson SG Standard is a legendary electric guitar with a fast, comfortable neck and powerful sound. Ideal for rock and metal musicians.",
   },
   {
+    id: "0323",
     name: "Epiphone Les Paul Standard",
     brand: "Epiphone",
     price: 499.99,
     type: "electric",
-    image: "https://images.tcdn.com.br/img/img_prod/1139574/guitarra_epiphone_les_paul_standard_60s_ebony_16247_3_ead5640cddc23bde7db47f241cb11a27.jpg",
+    image:
+      "https://images.tcdn.com.br/img/img_prod/1139574/guitarra_epiphone_les_paul_standard_60s_ebony_16247_3_ead5640cddc23bde7db47f241cb11a27.jpg",
     shortDescription: "Affordable electric guitar.",
     longDescription:
       "The Epiphone Les Paul Standard is a budget-friendly electric guitar with a warm, rich sound and versatile tone. Ideal for a range of musical styles.",
   },
   {
+    id: "0324",
     name: "Yamaha Pacifica 112V",
     brand: "Yamaha",
     price: 299.99,
     type: "electric",
-    image: "https://www.woodswindandbrass.com.au/wp-content/uploads/2021/09/PAC112V-Silver.jpg",
+    image:
+      "https://www.woodswindandbrass.com.au/wp-content/uploads/2021/09/PAC112V-Silver.jpg",
     shortDescription: "Affordable electric guitar.",
     longDescription:
       "The Yamaha Pacifica 112V is a budget-friendly electric guitar with a fast, comfortable neck and versatile sound. Ideal for a range of musical styles.",
   },
   {
+    id: "0325",
     name: "Fender Telecaster",
     brand: "Fender",
     price: 799.99,
     type: "electric",
-    image: "https://img.kytary.com/eshop_ie/velky_v2/na/637704250803000000/32e85909/64917400/fender-player-telecaster-mn-tpl.jpg",
+    image:
+      "https://img.kytary.com/eshop_ie/velky_v2/na/637704250803000000/32e85909/64917400/fender-player-telecaster-mn-tpl.jpg",
     shortDescription: "Classic electric guitar.",
     longDescription:
       "The Fender Telecaster is a classic electric guitar with a fast, comfortable neck and versatile sound. Ideal for a range of musical styles.",
   },
   {
+    id: "0326",
     name: "Gibson Les Paul",
     brand: "Gibson",
     price: 1499.99,
@@ -130,6 +153,7 @@ export default guitars = [
       "The Gibson Les Paul is a legendary electric guitar with a warm, rich sound and versatile tone. Ideal for a range of musical styles.",
   },
   {
+    id: "0327",
     name: "Ibanez RG 550",
     brand: "Ibanez",
     price: 799.99,
@@ -140,21 +164,25 @@ export default guitars = [
       "The Ibanez RG 550 is a high-performance electric guitar with a fast, smooth neck and versatile sound. Ideal for metal and shred guitarists.",
   },
   {
+    id: "0330",
     name: "ESP LTD EC-1000",
     brand: "ESP",
     price: 999.99,
     type: "electric",
-    image: "https://www.guitarlizard.com/wp-content/uploads/2021/12/ESP-LTD-EC-1000-Deluxe.jpg",
+    image:
+      "https://www.guitarlizard.com/wp-content/uploads/2021/12/ESP-LTD-EC-1000-Deluxe.jpg",
     shortDescription: "Professional electric guitar.",
     longDescription:
       "The ESP LTD EC-1000 is a professional electric guitar with a solid construction and powerful sound. Ideal for metal, rock, and blues musicians.",
   },
   {
+    id: "0331",
     name: "PRS SE Custom 24",
     brand: "PRS",
     price: 999.99,
     type: "electric",
-    image: "https://www.lojaopenstage.com.br/upload/product/V0qJ5cGxT3jw2rHWKkK052b6HvAo6JFY4gNwz8IW.jpg",
+    image:
+      "https://www.lojaopenstage.com.br/upload/product/V0qJ5cGxT3jw2rHWKkK052b6HvAo6JFY4gNwz8IW.jpg",
     shortDescription: "Stylish electric guitar.",
     longDescription:
       "The PRS SE Custom 24 is a stylish electric guitar with a warm, rich sound and versatile tone. Ideal for a range of musical styles.",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Category from "../components/Category";
+import Hero from "../components/Hero";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+      <Category />
+    </div>
+  );
 }
 
 export default Home;
