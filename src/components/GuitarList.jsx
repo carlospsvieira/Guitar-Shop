5d22d9a2340f0f731c80d304cd4bcf3d5cd47123
Carlos Vieira <carlepsvieira@gmail.com>
2023-02-10 19:13:16 -0300

    link to details page route set with id

--- src/components/GuitarList.jsx
+++ src/components/GuitarList.jsx
@@ -13,16 +13,33 @@ function GuitarList() {
     <div className="m-2">
       <div className="flex flex-wrap justify-center">
         {guitarList.map((guitar) => (
-          <div key={guitar.id} className="flex items-center">
-            <img src={guitar.image} alt="" className="w-[200px]"/>
+          <div className="flex flex-col justify-center">
+            <div>
+              <img src={guitar.image} alt="" className="w-[200px]" />
+            </div>
+            <div className="flex">
+              <Link
+                to={`${location.pathname}/${guitar.id}`}
+                className="bg-zinc-800 w-[100px] text-center text-white text-sm rounded-md py-1 px-2"
+              >
+                View Guitar
+              </Link>
+              <Link to="/cart" className="bg-amber-400 px-2 py-1 rounded-sm">Cart</Link>
+            </div>
           </div>
         ))}
       </div>
-      { location.pathname === '/acoustic' ? (
-        <Link to="/electric" className="bg-red-400 rounded-md px-2 py-1">See our electric guitars</Link>
-      ) : (
-        <Link to="/acoustic" className="bg-sky-500 rounded-md px-2 py-1">See our acoustic guitars</Link>
-      )}
+      <div className="mt-3">
+        {location.pathname === "/acoustic" ? (
+          <Link to="/electric" className="bg-red-400 rounded-md px-2 py-1">
+            See our electric guitars
+          </Link>
+        ) : (
+          <Link to="/acoustic" className="bg-sky-500 rounded-md px-2 py-1">
+            See our acoustic guitars
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
