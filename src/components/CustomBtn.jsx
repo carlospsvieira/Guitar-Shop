3682b12836c98db0ce98d89a0f186e93d7556453
Carlos Vieira <carlepsvieira@gmail.com>
2023-02-12 20:46:57 -0300

    localStorage setup

--- src/components/CustomBtn.jsx
+++ src/components/CustomBtn.jsx
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../context/AddProvider";
 
 function CustomBtn({ productId }) {
-  const { cart, setCart } = useContext(Context);
+  const { setCart } = useContext(Context);
   const [btnText, setBtnText] = useState("Add");
 
   // changes btn text on click back and forth //
@@ -14,25 +14,51 @@ function CustomBtn({ productId }) {
     }
   };
 
-  // adds target id to cart //
-  const addToCart = () => {
-    const existingId = cart.find((id) => id === productId);
-    const removeFromList = cart.filter((id) => id !== productId);
+  // handles btn previous btn text when page is reloaded //
+  const handleBtnText = () => {
+    const storage = JSON.parse(localStorage.getItem("cart"));
+    const existingId = storage?.find((id) => id === productId);
     if (existingId) {
-      setCart(removeFromList);
+      setBtnText("Added");
     } else {
-      setCart((prev) => [...prev, productId]);
+      setBtnText("Add");
     }
   };
 
-  const handleAdd = () => {
-    addToCart();
-    btnTextChange();
+  // adds and removes ids to localStorage
+  const handleStorage = () => {
+    let storage = JSON.parse(localStorage.getItem("cart"));
+    // checks for ids in localStorage
+    if (!storage) {
+      storage = [];
+    } else if (storage.some((id) => id === productId)) {
+      const keptItems = storage.filter((id) => id !== productId);
+      return localStorage.setItem("cart", JSON.stringify(keptItems));
+    }
+    // adds id to local storage //
+    storage.push(productId);
+    localStorage.setItem("cart", JSON.stringify(storage));
   };
+
+  // adds and removes target id to cart //
+  const handleCart = () => {
+    const storage = JSON.parse(localStorage.getItem("cart"));
+    setCart(storage);
+  };
+
+  const handleClick = () => {
+    handleStorage();
+    btnTextChange();
+    handleCart();
+  };
+
+  useEffect(() => {
+    handleBtnText();
+  }, []);
 
   return (
     <div>
-      <button onClick={handleAdd}>{btnText}</button>
+      <button onClick={handleClick}>{btnText}</button>
     </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,21 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../context/AddProvider";
 import { guitars } from "../data";
 
 function Cart() {
   const { cart, setCart } = useContext(Context);
+  const [empty, setEmpty] = useState(false);
   const newList = guitars.filter((guitar) =>
     cart?.map((id) => id).includes(guitar.id)
   );
 
+  const handleCart = () => {
+    const storage = JSON.parse(localStorage.getItem("cart"));
+    if (!storage) {
+      setEmpty(true);
+    } else {
+      setCart(storage);
+    }
+    return;
+  };
+
+  useEffect(() => {
+    handleCart();
+  }, []);
+
   return (
     <div>
       <h1>Cart</h1>
-      {newList.map((guitar) => (
-        <div key={guitar.id}>
-          <img src={guitar.image} alt="" className="w-[100px]" />
-        </div>
-      ))}
+      {empty
+        ? <p>Your cart is empty.</p>
+        : newList.map((guitar) => (
+            <div key={guitar.id}>
+              <img src={guitar.image} alt="" className="w-[100px]" />
+            </div>
+          ))}
     </div>
   );
 }
