694723ab7082ba79df83a308f865ec8839479231
Carlos Vieira <carlepsvieira@gmail.com>
2023-02-19 19:29:17 -0300

    style hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,24 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="h-screen hero">
-        <a href="#category" className="bg-amber-500 rounded-sm text-white text-center">
-          Start Shopping
-        </a>
+    <div className="h-screen hero flex flex-col">
+      <h1 className="text-white text-5xl mt-[55%] ml-[10%] sm:text-5xl sm:mt-[20%]">Acoustic and Electric Guitars</h1>
+      <a
+        href="#category"
+        className="bg-amber-500
+          py-2
+          rounded-sm
+          text-white
+          font-bold
+          text-center
+          text-xl
+          w-[250px]
+          mt-1
+          ml-[10%]
+          "
+      >
+        <span>Start Shopping</span>
+      </a>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Context } from "../context/AddProvider";
 import { CgProfile } from "react-icons/cg";
+import { RiShoppingCartLine } from "react-icons/ri"
+import { FaGuitar } from "react-icons/fa"
 
 function Navbar() {
   const { user, setUser } = useContext(Context);
@@ -17,27 +19,29 @@ function Navbar() {
   }, []);
 
   return (
-    <div className="flex items-center bg-black text-white">
-      <Link to="/" className="text-2xl mr-5">
-        Guitar Shop
+    <div className="flex justify-around items-center bg-black text-white py-4 w-full">
+      <Link to="/" className="text-2xl mr-5 flex">
+        <FaGuitar size={15}/> Guitar Shop
       </Link>
 
-      {location.pathname === "/login" ? (
-        // Checks whether user is at the login page or not.
-        // if so, user cannot see links to login or profile pages //
-        ""
-      ) : user && user.email.length > 0 ? (
-        // Checks if user is loggedin and display links //
-        <Link to="/profile" className="m-3">
-          <CgProfile size={25} />
-        </Link>
-      ) : (
-        <Link to="/login" className="m-3">
-          Login
-        </Link>
-      )}
+      <div className="flex items-center gap-8">
+        {location.pathname === "/login" ? (
+          // Checks whether user is at the login page or not.
+          // if so, user cannot see links to login or profile pages //
+          ""
+        ) : user && user.email.length > 0 ? (
+          // Checks if user is loggedin and display links //
+          <Link to="/profile">
+            <CgProfile size={25} />
+          </Link>
+        ) : (
+          <Link to="/login">
+            Login
+          </Link>
+        )}
 
-      <Link to="/cart">Cart</Link>
+        <Link to="/cart"><RiShoppingCartLine size={25}/></Link>
+      </div>
     </div>
   );
 }
