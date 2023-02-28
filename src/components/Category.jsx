fbeb025ccdf151ef7af093262f43b2be41d1cfb9
Carlos Vieira <carlepsvieira@gmail.com>
2023-02-28 19:19:09 -0300

    adds react animation on scroll hero and category sections

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 function Category() {
   return (
-    <div
+    <section
       id="category"
       className="flex flex-col items-center pt-[10%] gap-16 h-screen bg-slate-100"
     >
       <div>
         <h1 className="text-5xl">Your choice is what makes us safe & sound.</h1>
       </div>
-      <div className="flex gap-8 category-btns">
-        <Link to="acoustic" className="w-[fit-content]">
-          <div className="acoustic-btn">
-            <span>Acoustic</span>
-          </div>
-        </Link>
-        <Link to="electric" className="w-[fit-content]">
-          <div className="electric-btn">
-            <span>Electric</span>
-          </div>
-        </Link>
-      </div>
-    </div>
+      <AnimationOnScroll animateIn="animate__fadeIn">
+        <div className="flex gap-8 category-btns">
+          <Link to="acoustic" className="w-[fit-content]">
+            <div className="acoustic-btn">
+              <span>Acoustic</span>
+            </div>
+          </Link>
+          <Link to="electric" className="w-[fit-content]">
+            <div className="electric-btn">
+              <span>Electric</span>
+            </div>
+          </Link>
+        </div>
+      </AnimationOnScroll>
+    </section>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,29 @@
 import React from "react";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 function Hero() {
   return (
-    <div className="h-screen hero flex flex-col">
+    <div className="h-screen hero flex flex-col gap-4">
       <h1 className="text-white text-5xl mt-[55%] ml-[10%] sm:text-7xl sm:mt-[20%]">
         Acoustic and Electric Guitars
       </h1>
-      <a
-        href="#category"
-        className="bg-amber-500
+      <AnimationOnScroll animateIn="animate__fadeInLeft">
+        <a
+          href="#category"
+          className="bg-amber-500
           py-4
+          px-8
           rounded-md
           text-white
           font-bold
           text-center
           text-2xl
-          w-[250px]
-          mt-1
           ml-[10%]
           "
-      >
-        <span>Start Shopping</span>
-      </a>
+        >
+          <span>Start Shopping</span>
+        </a>
+      </AnimationOnScroll>
     </div>
   );
 }
