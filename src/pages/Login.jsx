48370768cd04f4d6f0a6dfcb2e49d3b97523b663
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-01 23:14:41 -0300

    login page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,31 +14,33 @@ function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-2 h-[50vh]">
+    <div className="flex flex-col items-center justify-center gap-1 h-[50vh]">
       <input
         type="email"
         name="email"
-        className="border-2 border-black"
+        className="border-2 border-black text-center py-2"
         onChange={(e) => setUser({ email: e.target.value })}
+        placeholder="Email"
       />
-      <span className="text-sm">email: adm@guitar-shop.com</span>
       <input
         type="password"
         name="password"
         onChange={(e) => setPassword(e.target.value)}
-        className="border-2 border-black"
+        className="border-2 border-black text-center py-2"
+        placeholder="Password"
       />
+      <span className="text-sm">email: adm@guitar-shop.com</span>
       <span className="text-sm">password: 1234567</span>
       <button
         onClick={handleClick}
         disabled={validPassword}
-        className="px-2 py-1"
+        className="px-4 py-2 text-white text-sm bg-black mt-5 mb-2 rounded-md"
       >
         Login
       </button>
       <div>
         <span className="text-sm">
-          Not a member yet? <Link to="/register">Sign Up</Link>
+          Not a member yet? <Link to="/register" className="underline">Sign Up</Link>
         </span>
       </div>
     </div>
