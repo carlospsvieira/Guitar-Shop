aa9617f64ead567fe949f47ee33335d0ff0993e6
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-14 14:41:25 -0300

    addapts background hero image to smaller screens changing the image to vertical position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,36 +1,35 @@
 import React from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
-import { Link } from 'react-scroll'
-import { TbDiscount2 } from "react-icons/tb";
+import { Link } from "react-scroll";
 
 function Hero() {
   return (
     <div className="h-[80vh] sm:h-screen hero flex flex-col gap-4">
-      <h1 className="text-black text-[4rem] ml-[15%] sm:text-[12rem] sm:font-extralight mt-[10rem] sm:ml-[10%]">
+      <h1 className="text-black text-[4rem] text-center sm:text-[12rem] sm:font-extralight mt-[10rem] sm:ml-[10%]">
         GUITARS
       </h1>
       <AnimationOnScroll animateIn="animate__fadeInLeft">
-      <Link
-        activeClass="active"
-        to="category"
-        spy={true}
-        duration={500}
-        className="bg-amber-600
+        <Link
+          activeClass="active"
+          to="category"
+          spy={true}
+          duration={500}
+          className="bg-amber-600
         py-3
         rounded-md
         text-white
         text-center
         text-xl
         font-[500]
-        ml-[15%]
+        ml-[4rem]
         sm:ml-[10%]
         cursor-pointer
         px-[3rem]
         sm:px-[17.5rem]
         sm:text-2xl
         "
-      >
-        Start Shopping
+        >
+          Start Shopping
         </Link>
       </AnimationOnScroll>
     </div>
