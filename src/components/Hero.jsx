c7866153ac599fe9d5320ff7f3b451a0a02f782d
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-14 14:44:19 -0300

    hero h1 back to start position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 function Hero() {
   return (
     <div className="h-[80vh] sm:h-screen hero flex flex-col gap-4">
-      <h1 className="text-black text-[4rem] text-center sm:text-[12rem] sm:font-extralight mt-[10rem] sm:ml-[10%]">
+      <h1 className="text-black text-[4rem] text-center sm:text-[12rem] sm:font-extralight mt-[10rem] sm:ml-[10%] sm:text-start">
         GUITARS
       </h1>
       <AnimationOnScroll animateIn="animate__fadeInLeft">
